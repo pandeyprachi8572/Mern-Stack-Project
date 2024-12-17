9ae204d49754e9879990644c975b701aa6bee7c3
Prachi Pandey <pandeyprachi8572@gmail.com>
2024-12-17 16:00:09 +0530

    *

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import { FaSearch } from 'react-icons/fa'
 export default function Header() {
-  return (
+return (
 <header className='bg-slate-200 shadow-md'>
-    <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-    <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-        <span className='text-slate-500'> Prachi </span>
-        <span className='text-slate-500'> Pandey </span>
-         </h1>
-         <form className='bg-slate-100 p-3 rounded-lg'>
-            <input type='text' placeholder='Search...' className='bg-transparent'/>
-         </form>
-         </div>
+<div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+<h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+<span className='text-slate-500'> Prachi </span>
+<span className='text-slate-500'> Pandey </span>
+</h1>
+         <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+            <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64 '/>
+            <FaSearch className="text-slate-600"/>
+</form>
+</div>
 </header>
   )
 }
